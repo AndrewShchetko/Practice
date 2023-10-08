d4434eff590535ef091cfa7ddceabae3a9185b24
AlbaLilium <flos.alba.lilium@gmail.com>
2023-10-08 15:51:55 +0300

    refactor(image): clean and encapsulate image getter

--- Demonstration/demapp/image.py
+++ Demonstration/demapp/image.py
@@ -1,1 +1,20 @@
 from PIL import Image
+import pandas as pd
+import numpy as np
+import numpy.typing as npt
+
+SIZEIMG = (244, 244)
+
+train_dataset = pd.read_csv("train.csv")
+test_dataset = pd.read_csv("test.csv")
+
+
+def get_img_dataset(dataset=train_dataset) -> npt.DTypeLike:
+    img_arr = dataset[' pixels'].copy()
+    for i in range(len(img_arr)):
+        img_arr[i] = np.fromstring(img_arr[i], dtype=np.uint8, sep=' ')
+        img_arr[i] = np.reshape(img_arr[i], (48, 48))
+    return img_arr.to_numpy()
+
+
+images_arr = get_img_dataset(train_dataset)
