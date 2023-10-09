339a123f130d7fbd76808a8a8be1517cffe4b8d6
AlbaLilium <flos.alba.lilium@gmail.com>
2023-10-09 21:26:07 +0300

    refactor(image): change type hint and rename inner variable in image getter

--- Demonstration/demapp/image.py
+++ Demonstration/demapp/image.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import pandas as pd
 import numpy as np
-import numpy.typing as npt
 
 SIZEIMG = (224, 224)
 
@@ -9,19 +8,19 @@ train_dataset = pd.read_csv("train.csv")
 test_dataset = pd.read_csv("test.csv")
 
 
-def get_img_dataset(dataset=train_dataset) -> npt.DTypeLike:
-    img_arr = dataset[' pixels'].copy()
-    for i in range(len(img_arr)):
-        img_arr[i] = np.fromstring(img_arr[i], dtype=np.uint8, sep=' ')
-        img_arr[i] = np.reshape(img_arr[i], (48, 48))
-    return np.stack(img_arr.to_numpy())
+def get_img_dataset(dataset: pd.DataFrame = train_dataset) -> np.ndarray:
+    image_df: pd.DataFrame = dataset[' pixels'].copy()
+    for i in range(len(image_df)):
+        image_df[i] = np.fromstring(image_df[i], dtype=np.uint8, sep=' ')
+        image_df[i] = np.reshape(image_df[i], (48, 48))
+    return np.stack(image_df.to_numpy())
 
 
 images_arr = get_img_dataset(train_dataset)
 
 
-def resize_img(img_arr: npt.DTypeLike) -> npt.DTypeLike:
-    resized_images: npt.DTypeLike = np.empty((img_arr.shape[0],*SIZEIMG))
+def resize_img(img_arr: np.ndarray) -> np.ndarray:
+    resized_images: np.ndarray = np.empty((img_arr.shape[0], *SIZEIMG))
     for i in range(img_arr.shape[0]):
         image = Image.fromarray(img_arr[i], mode='L')
         resized_images[i] = np.asarray(image.resize(SIZEIMG))
