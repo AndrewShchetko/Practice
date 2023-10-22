6d6880a67df9a988cfef5fe8355c9cbaedfdca27
AndrewShchetko <andronshch2003@gmail.com>
2023-10-22 16:56:25 +0300

    perf: change NN architecture to pretrained

--- Demonstration/demapp/neuralNetwork.py
+++ Demonstration/demapp/neuralNetwork.py
@@ -1,16 +1,19 @@
-from typing import Any
+from numpy.typing import NDArray
 import numpy as np
 from tensorflow import keras
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from image import get_img_dir_generator
+from keras.applications.vgg19 import VGG19, preprocess_input
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model
+from keras.optimizers import Adam
+from image import
 
 
 def make_image_array(images: list, emotions: list, directory: str):
-    for emotion, img in get_img_dir_generator(directory):
+    for emotion, img in (directory):
         emotions.append(emotion)
         images.append(img)
-    images: Any = np.array(images)
-    emotions: Any = keras.utils.to_categorical(emotions, 7)
+    images: NDArray = np.array(images)
+    emotions: NDArray = keras.utils.to_categorical(emotions, 7)
     return images, emotions
 
 
@@ -18,42 +21,29 @@ img_train, emotions_train_categorical = make_image_array([], [], 'data/train/')
 print(img_train.shape)
 img_test, emotions_test_categorical = make_image_array([], [], 'data/test/')
 print(img_test.shape)
+print('data parsed correctly')
+img_train = preprocess_input(img_train)
+img_test = preprocess_input(img_test)
 
-model = keras.Sequential([
-    Conv2D(64, (3, 3), padding='same', activation='relu', input_shape=(224, 224, 3)),
-    Conv2D(64, (3, 3), padding='same', activation='relu'),
-    MaxPooling2D(pool_size=(2, 2), strides=2),
-    Conv2D(128, (3, 3), padding='same', activation='relu'),
-    Conv2D(128, (3, 3), padding='same', activation='relu'),
-    MaxPooling2D(pool_size=(2, 2), strides=2),
-    Conv2D(256, (3, 3), padding='same', activation='relu'),
-    Conv2D(256, (3, 3), padding='same', activation='relu'),
-    Conv2D(256, (3, 3), padding='same', activation='relu'),
-    Conv2D(256, (3, 3), padding='same', activation='relu'),
-    MaxPooling2D(pool_size=(2, 2), strides=2),
-    Conv2D(512, (3, 3), padding='same', activation='relu'),
-    Conv2D(512, (3, 3), padding='same', activation='relu'),
-    Conv2D(512, (3, 3), padding='same', activation='relu'),
-    Conv2D(512, (3, 3), padding='same', activation='relu'),
-    MaxPooling2D(pool_size=(2, 2), strides=2),
-    Conv2D(512, (3, 3), padding='same', activation='relu'),
-    Conv2D(512, (3, 3), padding='same', activation='relu'),
-    Conv2D(512, (3, 3), padding='same', activation='relu'),
-    Conv2D(512, (3, 3), padding='same', activation='relu'),
-    MaxPooling2D(pool_size=(2, 2), strides=2),
-    Flatten(),
-    Dense(4069, activation='relu'),
-    Dense(4069, activation='relu'),
-    Dense(7, activation='softmax')
-])
+base_model = VGG19(weights='imagenet', include_top=False)
+
+out = base_model.output
+out = GlobalAveragePooling2D()(out)
+out = Dense(1024, activation='relu')(out)
+out = Dense(512, activation='relu')(out)
+predictions = Dense(7, activation='softmax')(out)
+
+model = Model(inputs=base_model.input, outputs=predictions)
+for layer in base_model.layers:
+    layer.trainable = False
 
 model.compile(
-    optimizer='adam',
+    optimizer=Adam(learning_rate=0.001),
     loss='categorical_crossentropy',
     metrics=['accuracy']
 )
 
-his = model.fit(img_train, emotions_train_categorical, batch_size=19, epochs=3, validation_split=0.2)
+his = model.fit(img_train, emotions_train_categorical, batch_size=32, epochs=10, validation_split=0.2)
 model.save('model')
-model_loaded = keras.models.load_model('model')
-model.evaluate(img_test, emotions_test_categorical)
+# model_loaded = keras.models.load_model('model')
+# model.evaluate(img_test, emotions_test_categorical)
