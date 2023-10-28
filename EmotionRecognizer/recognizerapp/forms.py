a0bfc9e484d0975a0ab1c9069de0db7996ffa828
AndrewShchetko <andronshch2003@gmail.com>
2023-10-28 13:44:21 +0300

    build: move forms and models from recognizer to personal account

--- EmotionRecognizer/recognizerapp/forms.py
+++ EmotionRecognizer/recognizerapp/forms.py
@@ -1,27 +1,5 @@
 from django import forms
-from .models import *
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
-
-class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-
-    class Meta:
-        model = User
-        fields = ('username', 'password1', 'password2')
-
-
-class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-
-
-class ChangePasswordForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
-    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    new_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+from .models import Images
 
 
 class NeuralNetworkForm(forms.ModelForm):
--- EmotionRecognizer/recognizerapp/models.py
+++ EmotionRecognizer/recognizerapp/models.py
@@ -1,13 +1,6 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
 class Images(models.Model):
     image = models.ImageField(upload_to='%Y/%m/%d')
     comment = models.CharField(max_length=255, blank=True)
-
-
-class Results(models.Model):
-    image = models.OneToOneField(Images, on_delete=models.CASCADE)
-    emotion = models.CharField(max_length=40)
-    user = models.CharField(max_length=100)
